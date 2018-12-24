fc2370e797d0b505514ad2af7db6bf8cedf7941e
albertgoncalves <goncalves.albert@gmail.com>
2018-12-24 15:35:18 -0500

    Whoops!

--- models/mark_and_recapture/mark_and_recapture_data.ml
+++ models/mark_and_recapture/mark_and_recapture_data.ml
@@ -4,29 +4,6 @@ module R = Random
 module S = String
 module U = Utils
 
-type data =
-    | Int of string * int
-    | IntList of string * int list
-    | FloatList of string * float list
-
-let export data =
-    let indiv to_string k v =
-        let v = to_string v in
-        P.sprintf "%s <- %s" k v in
-    let vector to_string k v =
-        let v = S.concat ", " @@ L.map to_string v in
-        P.sprintf "%s <- c(%s)" k v in
-    let export = function
-        | Int (k, v) -> indiv string_of_int k v
-        | IntList (k, v) -> vector string_of_int k v
-        | FloatList (k, v) -> vector string_of_float k v in
-    export data
-
-let write_to_file ~filename lines =
-    let out_channel = open_out filename in
-    L.iter (fun line -> P.fprintf out_channel "%s\n" line) lines;
-    close_out out_channel
-
 let histogram xs =
     let xs = L.sort compare xs in
     let rec loop count accu = function
