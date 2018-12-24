ed37c644d78e12e59030f32805eb353912243461
albertgoncalves <goncalves.albert@gmail.com>
2018-12-24 12:47:27 -0500

    Forgot this too!

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs; mkShell {
+    name = "stan";
+    buildInputs = [ ocaml-ng.ocamlPackages_4_07.ocaml
+                    ocaml-ng.ocamlPackages_4_07.findlib
+                    ocaml-ng.ocamlPackages_4_07.ocp-indent
+                    ocaml-ng.ocamlPackages_4_07.utop
+                    python36
+                    python36Packages.numpy
+                    python36Packages.flake8
+                    python36Packages.csvkit
+                    fzf
+                  ];
+    shellHook = ''
+        strcd() { cd "$(dirname $1)"; }
+        withfzf() {
+            local h
+            h=$(fzf)
+            if (( $? == 0 )); then
+                $1 "$h"
+            fi
+        }
+
+        alias cdfzf="withfzf strcd"
+        alias vimfzf="withfzf vim"
+        alias flake8="flake8 --ignore E124,E128,E201,E203,E241,W503"
+
+        export -f withfzf
+    '';
+}
