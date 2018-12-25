9264177e8f97b5519c57111912ee265bd30ca313
albertgoncalves <goncalves.albert@gmail.com>
2018-12-24 22:51:53 -0500

    More camels.

--- models/german_tanks.sh
+++ models/german_tanks.sh
@@ -6,5 +6,10 @@ model="german_tanks"
 
 stan_path=$model
 stan_file=$model
+ocaml_file="$stan_file"_data
 
+cd $stan_path
+nix-shell ../../shell.nix --run "sh ../utils/ocaml.sh $ocaml_file"
+
+cd ../
 sh ../stan.sh $stan_path $stan_file
---
+++ models/german_tanks/german_tanks_data.ml
@@ -0,0 +1,16 @@
+module D = Data
+module L = List
+module R = Random
+
+let main () =
+    let pop = 1000 in
+    let n = 5 in
+    let obs = L.init n (fun _ -> R.int pop) in
+
+    let data_list = L.map D.export [ D.Int ("n", n)
+                                   ; D.IntList ("obs", obs)
+                                   ] in
+
+    D.write_to_file ~filename:"german_tanks.data.R" data_list
+
+let () = main ()
