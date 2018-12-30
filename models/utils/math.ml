5d0f5b81d419287385742f27cd95f1e8a007cf84
albertgoncalves <goncalves.albert@gmail.com>
2018-12-30 16:47:57 -0500

    Idioms

--- models/utils/math.ml
+++ models/utils/math.ml
@@ -4,7 +4,7 @@ module R = Random
 (* https://www.johndcook.com/blog/2010/06/14/generating-poisson-random-values/ *)
 
 let poisson ~theta =
-    let l = exp (-.theta) in
+    let l = exp @@ -.theta in
     let rec loop k p =
         if p > l then loop (k + 1) (p *. R.float 1.0) else k in
     loop 0 1.0
@@ -13,7 +13,7 @@ let poisson ~theta =
 
 let gaussian ~mu ~sigma =
     let u1 = R.float 1.0 and u2 = R.float 1.0 in
-    let r = sqrt ((-2.0) *. (log u1)) in
-    let theta = cos (F.pi *. 2.0 *. u2) in
+    let r = sqrt @@ -.2.0 *. (log u1) in
+    let theta = cos @@ F.pi *. 2.0 *. u2 in
     let z0 = r *. theta in
     (z0 *. sigma) +. mu
